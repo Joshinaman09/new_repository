d90c81670f526b8354d401eb179f769fc9129c75
Naman Joshi <Joshinaman09@gmail.com>
2024-08-13 10:14:40 +0530

    commit 2

--- test.py
+++ test.py
@@ -1,8 +1,17 @@
 a = 2
 b  = 6
+c = 10
+d =23
 
 def new_function(a,b):
     if a>b:
         print('out')
     else:
         print('in')
+
+def new_function_2(c,d):
+
+    if c<d:
+        print('Here')
+    else:
+        print('no')
